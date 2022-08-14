8ae9cea20e0d15747ef387ef7f8a406106426e5a
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-14 16:47:04 +0900

    checked

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
 import DateHead from "./component/DateHead";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
@@ -9,20 +9,43 @@ import TodoList from "./component/TodoList";
 function App() {
   const today = new Date();
   const [todos, setTodos] = useState([
-    {id: 1, text: '작업환경 설정', done: true},
-    {id: 2, text: '리액트 네이티브 기초 공부하기', done: false},
-    {id: 3, text: '투두리스트 만들어보기', done: false},
+    { id: 1, text: "작업환경 설정", done: true },
+    { id: 2, text: "리액트 네이티브 기초 공부하기", done: false },
+    { id: 3, text: "투두리스트 만들어보기", done: false },
   ]);
+
+  const onInsert = text => {
+    //새로 등록할 항목의 id 를 구합니다.
+    //등록된 항목 중에서 가장 큰 id를 구하고, 그 값에 1을 더합니다.
+    //만약 리스트가 비어있다면 1을 id로 사용합니다.
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+
+    setTodos(todos.concat(todo));
+  };
+
+  const onToggle = id => {
+    const nextTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, done: !todo.done } : todo,
+    );
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={["bottom"]} style={styles.block}>
         <KeyboardAvoidingView
           // behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-          behavior={Platform.select({ios : 'padding', android: undefined})}
+          behavior={Platform.select({ ios: "padding", android: undefined })}
           style={styles.avoid}>
           <DateHead date={today} />
-          {todos.length === 0 ? <Empty/> : <TodoList todos={todos} />}
-          <AddTodo />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} onToggle={onToggle} />}
+          <AddTodo onInsert={onInsert}/>
         </KeyboardAvoidingView>
 
       </SafeAreaView>
@@ -33,10 +56,10 @@ function App() {
 const styles = StyleSheet.create({
   block: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
-  avoid:{
-    flex :1
-  }
+  avoid: {
+    flex: 1,
+  },
 });
 export default App;
