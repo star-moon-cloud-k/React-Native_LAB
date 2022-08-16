511ad65bd78b9d74e2ab4dede7e6d9a117113b52
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-16 14:20:34 +0900

    eslint

--- ReactNativeLearn/.eslintrc.js
+++ ReactNativeLearn/.eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    semi: always,
+  },
 };
--- ReactNativeLearn/.prettierrc.js
+++ ReactNativeLearn/.prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
   trailingComma: 'all',
+  printWidth: 80,
 };
