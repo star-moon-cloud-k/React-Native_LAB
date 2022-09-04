eb5844be763013969458f31680a21d709bc902cb
star-moon-cloud-k <kspecial44@gmail.com>
2022-09-04 21:40:23 +0900

    Add Todo

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import DateHead from './components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 function App() {
   const today = new Date();
-  console.log(today);
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브', done: true},
+    {id: 3, text: 'todolist', done: true},
+  ]);
+
+  const onInsert = text => {
+    //새로 등록할 항목의 id를 구합니다.
+    //등록된 항목 중에서 가장 큰 id를 구하고, 그 값에 1을 더합니다.
+    //민약 리스트가 비어있다면 1을 id로 사용합니다.
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+    setTodos(todos.concat(todo));
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -15,8 +36,9 @@ function App() {
           behavior={Platform.OS === 'ios' ? 'padding' : undefined}
           style={styles.avoid}>
           <DateHead date={today} />
-          <Empty />
-          <AddTodo />
+          {/* <Empty /> */}
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
+          <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
---
+++ TodoApp/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {FlatList, View, Text, StyleSheet} from 'react-native';
+import TodoItem from './TodoItem';
+function TodoList({todos}) {
+  return (
+      <FlatList
+          ItemSeparatorComponent={()=><View style = {styles.seperator}/>}
+      style={styles.list}
+      data={todos}
+      renderItem={({item}) => (
+        <TodoItem id={item.id} text={item.text} done={item.done} />
+      )}
+      keyExtractor={item => item.id.toString()}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+  },
+  seperator: {
+    backgroundColor: '#F7F060',
+    height: 1,
+  },
+});
+
+export default TodoList;
