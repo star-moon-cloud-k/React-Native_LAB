e8a5f1b8716b6e6b0a1b2e28727849b0eb6c822d
star-moon-cloud-k <kspecial44@gmail.com>
2022-11-28 02:46:11 +0900

    ReactNavigation

--- LearnReactNavigation/App.js
+++ LearnReactNavigation/App.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  return <NavigationContainer>{/*내비이게이션 설정*/}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: '홈',
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{
+            headerBackVisible: false,
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity onPress={onPress}>
+                <Text>Left</Text>
+              </TouchableOpacity>
+            ),
+            headerTitle: ({children}) => (
+              <View>
+                <Text>{children}</Text>
+              </View>
+            ),
+            headerRight: () => (
+              <View>
+                <Text>Right</Text>
+              </View>
+            ),
+          }}
+        />
+      </Stack.Navigator>
+      {/*내비이게이션 설정*/}
+    </NavigationContainer>
+  );
 }
 
-export default App();
+export default App;
