979414e4307ffd806e37620c0a80fb361632146a
star-moon-cloud-k <kspecial44@gmail.com>
2022-11-28 20:16:26 +0900

    MainScreen

--- LearnReactNavigation/App.js
+++ LearnReactNavigation/App.js
@@ -1,61 +1,26 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
+import MainScreen from './screens/MainScreen';
 import DetailScreen from './screens/DetailScreen';
-import {View, Text, TouchableOpacity} from 'react-native';
-import HeaderlessScreen from './screens/HeaderlessScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator>
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: '홈',
-            headerStyle: {
-              backgroundColor: '#29b6f6',
-            },
-            headerTintColor: '#ffffff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontSize: 20,
-            },
-          }}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{
-            headerBackVisible: false,
-            headerLeft: ({onPress}) => (
-              <TouchableOpacity onPress={onPress}>
-                <Text>Left</Text>
-              </TouchableOpacity>
-            ),
-            headerTitle: ({children}) => (
-              <View>
-                <Text>{children}</Text>
-              </View>
-            ),
-            headerRight: () => (
-              <View>
-                <Text>Right</Text>
-              </View>
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="Headerless"
-          component={HeaderlessScreen}
+          name="Main"
+          component={MainScreen}
           options={{headerShown: false}}
         />
+        <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
-      {/*내비이게이션 설정*/}
     </NavigationContainer>
   );
 }
--- LearnReactNavigation/screens/HomeScreen.js
+++ LearnReactNavigation/screens/HomeScreen.js
@@ -4,21 +4,10 @@ import {View, Button} from 'react-native';
 function HomeScreen({navigation}) {
   return (
     <View>
+      <Button title="Drwaer열기" onPress={() => navigation.openDrawer()} />
       <Button
-        title="Detail 1 열기"
-        onPress={() => navigation.push('Detail', {id: 1})}
-      />
-      <Button
-        title="Detail 2 열기"
-        onPress={() => navigation.push('Detail', {id: 2})}
-      />
-      <Button
-        title="Detail 3 열기"
-        onPress={() => navigation.push('Detail', {id: 3})}
-      />
-      <Button
-        title="Headerless 열기"
-        onPress={() => navigation.push('Headerless')}
+        title="Setting 열기"
+        onPress={() => navigation.navigate('Setting')}
       />
     </View>
   );
